660d5d69b8ff53b3b09284e68831c6fab6eb342f
Colm Murphy <colmmurphy016@gmail.com>
2023-12-06 18:43:31 +0000

    modified ColourTable such that all tests now pass

--- src/main/java/xyz/colmmurphy/ColourTable.java
+++ src/main/java/xyz/colmmurphy/ColourTable.java
@@ -1,11 +1,12 @@
 package xyz.colmmurphy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ColourTable {
 
     private final int numColours;
-    private ArrayList<int[]> palette;
+    private final ArrayList<int[]> palette;
 
     public ColourTable(int numColours) {
         if (numColours <= 1 || numColours > 1024 || !isPowerOf2(numColours)) {
@@ -29,11 +30,36 @@ public class ColourTable {
     }
 
     public void add(int[] rgb) {
+        if (this.palette.size() == this.numColours) {
+            throw new ColourTableCapacityExceededException();
+        }
+        if (Arrays.stream(rgb).anyMatch((it) -> it < 0 || it > 255)) {
+            throw new IllegalArgumentException("Invalid RBG value(s)");
+        }
+        for (int[] colour : this.palette) {
+            if (Arrays.equals(colour, rgb)) {
+                throw new DuplicateColorException("Colour " + Arrays.toString(rgb) + " already exists in the palette");
+            }
+        }
         this.palette.add(rgb);
     }
 
     public void add(int red, int green, int blue) {
-        add(new int[] {red, green, blue});
-        char x = 255;
+        this.add(new int[] {red, green, blue});
+    }
+
+    public void add(String hexCode) {
+        if (!hexCode.matches("^#?[a-f0-9A-F]{6}$")) {
+            throw new IllegalArgumentException("not a valid Hex Code");
+        }
+        int startIndex = (hexCode.startsWith("#")) ? 1 : 0;
+        int red = Integer.parseInt(hexCode.substring(startIndex, startIndex + 2), 16);
+        int green = Integer.parseInt(hexCode.substring(startIndex + 2, startIndex + 4), 16);
+        int blue = Integer.parseInt(hexCode.substring(startIndex + 4, startIndex + 6), 16);
+        this.add(red, green, blue);
+    }
+
+    public int[] get(int index) {
+        return this.palette.get(index);
     }
 }
