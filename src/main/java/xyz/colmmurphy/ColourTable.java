f62289d65d2c102f3ee275ef2465a511c0f9d885
Colm Murphy <colmmurphy016@gmail.com>
2023-12-06 14:28:47 +0000

    write failing constructor test

--- src/main/java/xyz/colmmurphy/ColourTable.java
+++ src/main/java/xyz/colmmurphy/ColourTable.java
@@ -1,4 +1,27 @@
 package xyz.colmmurphy;
 
 public class ColourTable {
+
+    private int numColours;
+    public ColourTable(int numColours) {
+        this.setNumColours(numColours);
+    }
+
+    private boolean isPowerOf2(int x) {
+        int count = 0;
+        while (x > 0) {
+            count += x & 1;
+            x >>= 1;
+        }
+        return count == 1;
+    }
+    public int getNumColours() {
+        return this.numColours;
+    }
+
+    public void setNumColours(int numColours) {
+        if (numColours <= 1 || !isPowerOf2(numColours)) {
+            throw new IllegalArgumentException("numColours must be a power of 2 greater than 1");
+        }
+    }
 }
