b7bfc34346ed25c5a0c9c77971bddfcaa775c316
Colm Murphy <colmmurphy016@gmail.com>
2023-12-06 15:35:52 +0000

    write tests for add method

--- src/test/java/xyz/colmmurphy/ColourTableTest.java
+++ src/test/java/xyz/colmmurphy/ColourTableTest.java
@@ -6,9 +6,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTableTest {
+    private ColourTable ct;
+    private ColourTable fullColourTable;
     @BeforeEach
     public void beforeEach() {
-
+        ct = new ColourTable(16);
+        fullColourTable = new ColourTable(4);
+        fullColourTable.add(255, 0, 0);
+        fullColourTable.add(0, 255, 0);
+        fullColourTable.add(0, 0, 255);
+        fullColourTable.add(255, 255, 255);
     }
 
     @Test
@@ -22,5 +29,14 @@ class ColourTableTest {
                 "numColours must be a power of 2 less than 1025");
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(2048),
                 "numColours must be less than 1025");
+    }
+
+    @Test
+    public void testAdd() {
+        assertDoesNotThrow(() -> {ct.add((byte) 1, (byte) 2, (byte) 3);} );
+        assertThrows(ColourTableCapacityExceededException.class, () ->
+                fullColourTable.add((byte) 1, (byte) 2, (byte) 3),
+                "Cannot add colour to full colour palette"
+        );
     }
 }
